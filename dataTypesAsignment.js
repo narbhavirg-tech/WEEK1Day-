1f9cc1ea11d2f9a8312612b6bd59906300d6d397
narbhavirg-tech <narbhavirg@gmail.com>
2026-07-19 19:16:48 +0530

    Add files via upload
    Day01

---
+++ dataTypesAsignment.js
@@ -0,0 +1,15 @@
+var firstName,  companyName,  mobileNumber,  isAutomation , hasPlaywright;
+firstName = `Rajalakshmi`;
+companyName = `HCLTech`;
+mobileNumber = 1234567890;
+isAutomation = true;
+console.log("*****************************************************");
+console.log("Datatype    Value assigned");
+console.log("---------   ----------------")
+console.log(typeof firstName +"       " +firstName);
+console.log(typeof companyName+"       " + companyName);
+console.log(typeof mobileNumber+"       " + mobileNumber);
+console.log(typeof isAutomation+"      " + isAutomation);
+console.log(typeof hasPlaywright+"    " + hasPlaywright);
+console.log("*****************************************************");
+ 
